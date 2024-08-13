d3f57292d2a4600fba6370a95f82613b42173b9e
Mayank <kumar113349@gmail.com>
2024-08-13 15:22:48 +0530

    first commit

---
+++ src/components/Alert.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+export default function Alert(props) {
+  
+const capitalize = (word) => {
+    const txt = word.toLowerCase();
+    return txt.charAt(0).toUpperCase() + txt.slice(1);
+}
+  return (
+    props.alert && (
+      <div
+        className={`alert alert-${props.alert.type} alert-dismissible fade show`}
+        role="alert"
+      >
+        <strong>{capitalize(props.alert.type) + "!"}</strong> {props.alert.msg}
+      </div>
+    )
+  );    
+}
